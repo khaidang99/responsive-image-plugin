6610b2d9dc1c3e78a51f55ec2420c324c3c9a35d
KhaiDang <khaidang2812@gmail.com>
2022-12-13 23:17:24 +0700

    create: module

--- README.md
+++ README.md
@@ -1,36 +1,34 @@
-# Foobar
+# responsive-image-plugin
 
-Foobar is a Python library for dealing with word pluralization.
+A webpack plugin for responsive images.
 
 ## Installation
 
-Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
-
 ```bash
-pip install foobar
+npm install responsive-image-plugin sharp --save-dev
 ```
 
 ## Usage
 
-```python
-import foobar
+```js
+var OptimizeImagePlugin = require("responsive-image-plugin");
 
-# returns 'words'
-foobar.pluralize('word')
-
-# returns 'geese'
-foobar.pluralize('goose')
-
-# returns 'phenomenon'
-foobar.singularize('phenomena')
+module.exports = {
+  // ...
+  plugins: [
+    new OptimizeImagePlugin({
+      test: /\.(png|gif|ico|jpg|jpeg)$/,
+      options: {
+        sourcePath: "public",
+        outputPath: "public/assets/optimize-images",
+        sizes: [375, 768, 1280],
+        quality: 80,
+        overrideExtension: true,
+      },
+    }),
+  ],
+};
 ```
-
-## Contributing
-
-Pull requests are welcome. For major changes, please open an issue first
-to discuss what you would like to change.
-
-Please make sure to update tests as appropriate.
 
 ## License
 
--- index.js
+++ index.js
@@ -1,41 +1,204 @@
-const { resolve } = require("path");
-const fs = require("fs");
+const path = require("path");
+const sharp = require("sharp");
 
-function formatBytes(bytes, decimals = 2) {
-  if (!+bytes) return "0 Bytes";
+const CYAN = "\x1b[36m%s\x1b[0m";
+const GREEN = "\x1b[32m%s\x1b[0m";
+const RED = "\x1b[31m%s\x1b[0m";
 
-  const k = 1024;
-  const dm = decimals < 0 ? 0 : decimals;
-  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+class OptimizeImagePlugin {
+  static defaultOptions = {
+    sourcePath: "public/assets/images",
+    outputPath: "public/assets/optimize-images-generated",
+    sizes: [375, 768, 1280],
+    quality: 90,
+    overrideExtension: true,
+  };
 
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-
-  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
-}
-
-module.exports = class BundlesizeWebpackPlugin {
-  constructor(options) {
-    this.options = options || {
-      sizeLimit: 3,
-    };
+  // Any options should be passed in the constructor of your plugin,
+  // (this is a public API of your plugin).
+  constructor({ test = /\.(png|gif|ico|jpg|jpeg)$/, options = {} }) {
+    // Applying user-specified options over the default options
+    // and making merged options further available to the plugin methods.
+    // You should probably validate all the options here as well.
+    this.test = test;
+    this.options = { ...OptimizeImagePlugin.defaultOptions, ...options };
   }
+
   apply(compiler) {
-    compiler.hooks.done.tap("BundleSizePlugin", (stats) => {
-      const { path, filename } = stats.compilation.options.output;
-      const bundlePath = resolve(path, filename);
-      const { size } = fs.statSync(bundlePath);
-      const bundleSize = formatBytes(size);
-      console.log(bundleSize);
-      const { sizeLimit } = this.options;
-      if (bundleSize < sizeLimit) {
-        console.log("Safe:Bundle-Size", "\n SIZE LIMIT:", sizeLimit);
-      } else {
-        if (bundleSize === sizeLimit) {
-          console.warn("Warn:Bundle-Size", "\n SIZE LIMIT:", sizeLimit);
-        } else {
-          console.error("Unsafe:Bundle-Size", "\n SIZE LIMIT:", sizeLimit);
-        }
+    const pluginName = OptimizeImagePlugin.name;
+
+    // webpack module instance can be accessed from the compiler object,
+    // this ensures that correct version of the module is used
+    // (do not require/import the webpack or any symbols from it directly).
+    const { webpack, intermediateFileSystem } = compiler;
+
+    // Compilation object gives us reference to some useful constants.
+    const { Compilation } = webpack;
+
+    // RawSource is one of the "sources" classes that should be used
+    // to represent asset sources in compilation.
+    // const { RawSource } = webpack.sources;
+
+    const context = (
+      base = ".",
+      scanSubDirectories = false,
+      regularExpression = /\.js$/
+    ) => {
+      const files = {};
+
+      function readDirectory(directory) {
+        intermediateFileSystem.readdirSync(directory).forEach((file) => {
+          const fullPath = path.resolve(directory, file);
+
+          if (intermediateFileSystem.statSync(fullPath).isDirectory()) {
+            if (scanSubDirectories) readDirectory(fullPath);
+
+            return;
+          }
+
+          if (!regularExpression.test(fullPath)) return;
+
+          files[fullPath] = {
+            directory: directory,
+            fileName: file,
+          };
+        });
       }
+
+      readDirectory(base);
+      function Module(file) {
+        return files[file];
+      }
+
+      Module.keys = () => Object.keys(files);
+
+      return Module;
+    };
+
+    function resize({ size, imagePath, options }) {
+      return new Promise((resolve, reject) => {
+        let image = sharp(imagePath);
+        image.metadata().then((metadata) => {
+          let width = Math.min(metadata.width, size);
+          let height = Math.round((size / metadata.width) * metadata.height);
+          let resized = image.clone().resize(width, height);
+          resized = resized.webp({
+            // lossless: true, // Nén hình không làm mất dữ liệu ban đầu
+            quality: options.quality,
+          });
+
+          resized.toBuffer((err, data, { height }) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve({
+                data,
+                width,
+                height,
+              });
+            }
+          });
+        });
+      });
+    }
+
+    function writeFile(fs, filePath, data) {
+      return new Promise((resolve, reject) => {
+        fs.mkdir(path.dirname(filePath), { recursive: true }, (err) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+
+          fs.writeFile(filePath, data, (err) => {
+            if (err) throw err;
+
+            resolve(filePath);
+          });
+        });
+      });
+    }
+
+    const onEmit = (compilation, callback) => {
+      const contextImage = context(
+        path.join(__dirname, this.options.sourcePath),
+        true,
+        this.test
+      );
+      const assetNames = contextImage.keys();
+
+      console.log(CYAN, "OptimizeImagePlugin: Assets are being optimized....");
+
+      Promise.all(
+        assetNames.map((name) => {
+          let inputFilePath = name;
+          let outputFileName = contextImage(name).fileName;
+          if (this.options.overrideExtension) {
+            outputFileName = outputFileName.split(".").slice(0, -1).join(".");
+          }
+
+          const promises = [];
+          const promisesOptimize = [];
+
+          //optimize images
+          this.options.sizes.forEach((size) => {
+            promises.push(
+              resize({
+                size,
+                imagePath: inputFilePath,
+                options: {
+                  quality: this.options.quality,
+                },
+              })
+            );
+          });
+
+          try {
+            Promise.all(promises).then((resizes) => {
+              resizes.forEach((size) => {
+                let fileName = path.join(
+                  __dirname,
+                  this.options.outputPath,
+                  `${outputFileName}-${size.width}x${size.height}.webp`
+                );
+                promisesOptimize.push(
+                  writeFile(intermediateFileSystem, fileName, size.data)
+                );
+              });
+
+              Promise.all(promisesOptimize);
+            });
+          } catch (err) {
+            console.error(
+              RED,
+              `OptimizeImagePlugin: Something wrong when write files image sizes! ${err}`
+            );
+            Promise.reject(-1);
+          }
+
+          return Promise.resolve(0);
+        })
+      ).then((result) => {
+        console.log(
+          GREEN,
+          `OptimizeImagePlugin: Assets are finishing optimized!`
+        );
+        callback();
+      });
+    };
+
+    compiler.hooks.thisCompilation.tap(pluginName, (compilation) => {
+      compilation.hooks.processAssets.tapAsync(
+        {
+          name: pluginName,
+          // Using one of the later asset processing stages to ensure
+          // that all assets were already added to the compilation by other plugins.
+          stage: Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE,
+        },
+        (assets, callback) => onEmit(compilation, callback)
+      );
     });
   }
-};
+}
+
+module.exports = OptimizeImagePlugin;
