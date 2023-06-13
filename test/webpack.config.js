9ab2f0093fd8b01e7be5b53056fbce7824dec3c0
Hoang <cloudkick195@gmail.com>
2023-06-13 14:56:27 +0700

    config: overrideExtension

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 
-const OptimizeImagePlugin = require("../lib/index.js");
-// const OptimizeImagePlugin = require("../index.js");
+const OptimizeImagePlugin = require("../dist/index.js");
 
 module.exports = {
   mode: "development",
@@ -16,7 +15,7 @@ module.exports = {
       test: /\.(png|gif|ico|jpg|jpeg)$/,
       options: {
         sourcePath: path.resolve(__dirname, "public"),
-        outputPath: "test/optimize-images",
+        outputPath: path.resolve(__dirname, "optimize-images"),
         sizes: [375, 768, 1280],
         quality: 80,
         overrideExtension: true,
@@ -25,43 +24,3 @@ module.exports = {
   ],
   target: "node",
 };
-
-// const path = require("path");
-
-// module.exports = {
-//   mode: "development",
-//   entry: path.resolve(__dirname, "index"),
-//   module: {
-//     rules: [
-//       // This rule will be matched when the resourceQuery contains `minmax`, e.g. `cat-1000.jpg?minmax`
-//       {
-//         test: /\.(png|jpg)$/,
-//         resourceQuery: /minmax/,
-//         loader: require.resolve("../lib/index"),
-//         options: {
-//           min: 100,
-//           max: 300,
-//           esModule: true,
-//           adapter: require("../../jimp"),
-//         },
-//         type: "javascript/auto",
-//       },
-//       {
-//         test: /\.(png|jpg)$/,
-//         loader: require.resolve("../../lib/index"),
-//         options: {
-//           sizes: [500, 750, 1000],
-//           esModule: true,
-//           adapter: require("../../jimp"),
-//         },
-//         type: "javascript/auto",
-//       },
-//     ],
-//   },
-//   output: {
-//     path: path.resolve(__dirname, "build"),
-//     publicPath: "foobar/",
-//     filename: "test.js",
-//   },
-//   target: "node",
-// };
