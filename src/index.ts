092702125e79622fbc0cc4e6a3c937a22ca352ea
Hoang <cloudkick195@gmail.com>
2023-06-13 14:55:46 +0700

    update: write file

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 const path = require("path");
-const sharp = require("sharp");
+import sharp from "sharp";
+import fs from "fs";
 
-import { Metadata, OutputInfo } from "sharp";
 import { Compiler, Compilation } from "webpack";
 import { InnerCallback } from "tapable";
 
 const CYAN = "\x1b[36m%s\x1b[0m";
 const GREEN = "\x1b[32m%s\x1b[0m";
 const RED = "\x1b[31m%s\x1b[0m";
+
+interface ResizeResult {
+  data: Buffer;
+  width: number;
+  height: number;
+}
 
 export type OptionsPlugin = {
   sourcePath: string;
@@ -23,10 +29,10 @@ export type ParamsResize = {
   options?: OptionsPlugin;
 };
 
-class OptimizeImagePlugin {
+export class OptimizeImagePlugin {
   static defaultOptions: OptionsPlugin = {
-    sourcePath: "public/assets/images",
-    outputPath: "public/assets/optimize-images-generated",
+    sourcePath: undefined,
+    outputPath: undefined,
     sizes: [375, 768, 1280],
     quality: 90,
     overrideExtension: true,
@@ -48,7 +54,7 @@ class OptimizeImagePlugin {
   apply(compiler: Compiler) {
     const pluginName: string = OptimizeImagePlugin.name;
 
-    const { webpack, intermediateFileSystem } = compiler;
+    const { webpack } = compiler;
     const { Compilation } = webpack;
 
     const context = (
@@ -61,10 +67,9 @@ class OptimizeImagePlugin {
     } => {
       const files = {};
       function readDirectory(directory: string) {
-        const fileSystem: any = intermediateFileSystem;
-        fileSystem.readdirSync(directory).forEach((file: string) => {
+        fs.readdirSync(directory).forEach((file: string) => {
           const fullPath = path.resolve(directory, file);
-          if (fileSystem.statSync(fullPath).isDirectory()) {
+          if (fs.statSync(fullPath).isDirectory()) {
             if (scanSubDirectories) readDirectory(fullPath);
             return;
           }
@@ -86,20 +91,26 @@ class OptimizeImagePlugin {
       return Module;
     };
 
-    async function resize({ size, imagePath, options }: ParamsResize) {
-      return new Promise((resolve, reject) => {
-        let image = sharp(imagePath);
-        image.metadata().then((metadata: Metadata) => {
-          let width = Math.min(metadata.width, size);
-          let height = Math.round((size / metadata.width) * metadata.height);
-          let resized = image.clone().resize(width, height);
+    async function resize({
+      size,
+      imagePath,
+      options,
+    }: ParamsResize): Promise<ResizeResult> {
+      return new Promise<ResizeResult>((resolve, reject) => {
+        let image: sharp.Sharp = sharp(imagePath);
+        image.metadata().then((metadata: sharp.Metadata) => {
+          let width: number = Math.min(metadata.width, size);
+          let height: number = Math.round(
+            (size / metadata.width) * metadata.height
+          );
+          let resized: sharp.Sharp = image.clone().resize(width, height);
           resized = resized.webp({
             // lossless: true, // Nén hình không làm mất dữ liệu ban đầu
             quality: options.quality,
           });
 
           resized.toBuffer(
-            (err: Error, data: Buffer, { height }: OutputInfo) => {
+            (err: Error, data: Buffer, { height }: sharp.OutputInfo) => {
               if (err) {
                 reject(err);
               } else {
@@ -115,101 +126,96 @@ class OptimizeImagePlugin {
       });
     }
 
-    function writeFile(fs: any, filePath: string, data: Buffer) {
-      return new Promise((resolve, reject) => {
-        fs.mkdir(path.dirname(filePath), { recursive: true }, (err: Error) => {
-          if (err) {
-            reject(err);
-            return;
+    function writeFile(filePath: string, data: Buffer): Promise<string> {
+      return new Promise<string>((resolve, reject) => {
+        fs.mkdir(
+          path.dirname(filePath),
+          { recursive: true },
+          (err: Error | null) => {
+            if (err) {
+              reject(err);
+              return;
+            }
+
+            fs.writeFile(filePath, data, (err: Error | null) => {
+              if (err) throw err;
+
+              resolve(filePath);
+            });
           }
-
-          fs.writeFile(filePath, data, (err: Error) => {
-            if (err) throw err;
-
-            resolve(filePath);
-          });
-        });
+        );
       });
     }
 
     const onEmit = (callback: InnerCallback<Error, void>) => {
-      const contextImage = context(this.options.sourcePath, true, this.test);
-      const assetNames = contextImage.keys();
+      if (!this.options.sourcePath || !this.options.outputPath) {
+        console.log(CYAN, "sourcePath or outputPath undefine");
+        return;
+      }
+      const contextImage: {
+        (file: string): any;
+        keys(): string[];
+      } = context(this.options.sourcePath, true, this.test);
+
+      const assetNames: string[] = contextImage.keys();
 
       console.log(CYAN, "OptimizeImagePlugin: Assets are being optimized....");
 
-      Promise.all(
-        assetNames.map((name) => {
-          let inputFilePath = name;
-          let outputFileName = contextImage(name).fileName;
-          if (this.options.overrideExtension) {
-            outputFileName = outputFileName.split(".").slice(0, -1).join(".");
-          }
+      assetNames.map((name: string) => {
+        // thong bao thanh cong hay that bai
+        let inputFilePath: string = name;
+        let fileNameContext: string = contextImage(name).fileName;
+        if (this.options.overrideExtension) {
+          fileNameContext = fileNameContext.split(".").slice(0, -1).join("."); //image.img => image
+        }
 
-          const promises: any[] = [];
-          const promisesOptimize = [];
+        const promises: Promise<ResizeResult>[] = [];
 
-          //optimize images
-          this.options.sizes.forEach((size) => {
-            promises.push(
-              resize({
-                size,
-                imagePath: inputFilePath,
-                options: {
-                  ...this.options,
-                },
-              })
-            );
-          });
+        //optimize images
+        this.options.sizes.forEach((size) => {
+          promises.push(
+            resize({
+              size,
+              imagePath: inputFilePath,
+              options: {
+                ...this.options,
+              },
+            })
+          );
+        });
 
-          try {
-            Promise.all(promises).then((resizes) => {
-              resizes.forEach((size) => {
-                let fileName = path.join(
-                  this.options.outputPath,
-                  `${outputFileName}-${size.width}x${size.height}.webp`
-                );
-                promisesOptimize.push(
-                  writeFile(intermediateFileSystem, fileName, size.data)
-                );
-              });
-
-              Promise.all(promisesOptimize);
+        try {
+          Promise.all(promises).then((resizes) => {
+            resizes.forEach((size) => {
+              let fileName = path.join(
+                this.options.outputPath,
+                `${fileNameContext}-${size.width}x${size.height}.webp`
+              );
+              writeFile(fileName, size.data);
             });
-          } catch (err) {
-            console.error(
-              RED,
-              `OptimizeImagePlugin: Something wrong when write files image sizes! ${err}`
-            );
-            Promise.reject(-1);
-          }
-
-          return Promise.resolve(0);
-        })
-      ).then((result) => {
-        console.log(
-          GREEN,
-          `OptimizeImagePlugin: Assets are finishing optimized!`
-        );
-        callback();
+          });
+        } catch (err) {
+          console.error(
+            RED,
+            `OptimizeImagePlugin: Something wrong when write files image sizes! ${err}`
+          );
+          Promise.reject(-1);
+        }
       });
     };
 
-    compiler.hooks.thisCompilation.tap(
-      pluginName,
-      (compilation: Compilation) => {
-        compilation.hooks.processAssets.tapAsync(
-          {
-            name: pluginName,
-            // Using one of the later asset processing stages to ensure
-            // that all assets were already added to the compilation by other plugins.
-            stage: Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE,
-          },
-          (assets, callback) => onEmit(callback)
-        );
-      }
-    );
+    compiler.hooks.compilation.tap(pluginName, (compilation: Compilation) => {
+      compilation.hooks.processAssets.tapAsync(
+        {
+          name: pluginName,
+          // Using one of the later asset processing stages to ensure
+          // that all assets were already added to the compilation by other plugins.
+          stage: Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE,
+        },
+        (assets, callback) => onEmit(callback)
+      );
+    });
   }
 }
 
-export default OptimizeImagePlugin;
+module.exports = OptimizeImagePlugin;
